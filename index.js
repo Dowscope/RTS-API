745014392829ae350b05d602a227403662c84f3b
Timothy Dowling <timothy.dowling@me.com>
2025-02-18 14:37:06 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+
+app.get('/status', (req, res) => {
+  exec('systemctl is-active godot-server.service', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error checking service ${err.message}`);
+      return res.json({status: 'unknown'});
+    }
+    if (stderr) {
+      console.error(`Service stderr:  ${err.message}`);
+      return res.json({status: 'unknown'});
+    }
+    res.json({status: stdout.trim()});
+  });
+});
+
+app.listen(55000, () => console.log("Running on port: 55000"));
